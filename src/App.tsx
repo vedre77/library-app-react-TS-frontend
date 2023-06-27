de1584b488ddae568fee1f31e0b0c88768ef583d
vedre77 <vedranvitas@net.hr>
2023-06-27 17:05:39 +0200

    Added bootstrap icons, BookCheckoutPage and CheckoutAndReviewPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 export const App = () => {
   return (
-    <div>
-      <Navbar/>
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
   );
