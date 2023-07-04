340940358409d6c3dbcf5df8a59ff17ce5c3aec6
vedre77 <vedranvitas@net.hr>
2023-07-04 13:38:09 +0200

    Added admin models and pages

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import LoginWidget from './auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
+import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -55,7 +56,7 @@ export const App = () => {
             <Route path='/login/callback' component={LoginCallback} />
             <SecureRoute path='/shelf'> <ShelfPage/> </SecureRoute>
             <SecureRoute path='/messages'> <MessagesPage/> </SecureRoute>
-            {/* <SecureRoute path='/admin'> <ManageLibraryPage/> </SecureRoute> */}
+            <SecureRoute path='/admin'> <ManageLibraryPage/> </SecureRoute>
           </Switch>
         </div>
         <Footer />
